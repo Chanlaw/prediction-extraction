08c4bcac38d909d5e34ac6972293be77785cce56
Lawrence Chan <chanlaw@seas.upenn.edu>
2016-05-02 19:19:28 -0400

    Upload classifiers

---
+++ classifiers/svm_classifier.py
@@ -0,0 +1,79 @@
+import re
+import pandas as pd 
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.metrics import precision_recall_fscore_support
+
+data = pd.read_csv("cf_report.tsv", header=0, delimiter="\t", quoting=3)
+
+
+def sentence_to_words( sentence ):
+	# converts a raw sentence to a string of words delimited by spaces, with
+
+	letters_only = re.sub("[^a-zA-Z\s]", "", sentence)
+	words = letters_only.lower().split()
+	return( " ".join( words ))
+
+num_sentences = data["sentence"].size
+clean_sentences = []
+
+print "Cleaning sentences..."
+
+for i in xrange( 0, num_sentences):
+	if ( (i+1)%1000 == 0):
+		print "Cleaning sentence %d of %d" % ( i+1, num_sentences)
+	clean_sentences.append( sentence_to_words( data["sentence"][i]))
+
+print
+print "Creating the bag of words...\n"
+
+vectorizer = CountVectorizer(analyzer = "word", max_features = 1000)
+
+data_features = vectorizer.fit_transform(clean_sentences)
+data_features = data_features.toarray()
+
+
+accuracy = []
+for i in xrange(1,21):
+	#Perform 10-fold cross validation
+	X_train, X_test, y_train, y_test = train_test_split(data_features, data["label"], train_size = 0.9, random_state=14)
+	X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, train_size = 0.89)
+
+
+	max_accuracy = 0.0
+	best_C = 0.01
+	print "\n-"
+	print "Fold %d" % i
+	print "Training Support Vector Machine...\n"
+
+	for c in [ 0.01, 0.03, 0.05, 0.1, 0.3, 0.5, 1, 3, 5, 10]:
+		print "Validating with C=%.2f" %c
+		logistic = SVC( tol=1E-6, C=c, kernel='linear')
+		logistic = logistic.fit(X_train, y_train)
+		train_acc = logistic.score(X_train, y_train)
+		print "Train Accuracy %.05f" % train_acc
+		valid_acc = logistic.score(X_valid, y_valid)
+		print "Validation Accuracy %.05f" % valid_acc
+		if ( valid_acc > max_accuracy ):
+			best_C = c
+			max_accuracy = valid_acc
+
+	print
+	print "Training final Support Vector Machine model with C=%.2f" %best_C
+	logistic = SVC( tol=1E-6, C=best_C, kernel = 'linear')
+	logistic = logistic.fit(X_train, y_train)
+	train_acc = logistic.score(X_train, y_train)
+	print "Train Accuracy %.05f" % train_acc
+	valid_acc = logistic.score(X_valid, y_valid)
+	print "Valid Accuracy %.05f" % valid_acc
+	test_acc = logistic.score(X_test, y_test)
+	print "Test Accuracy %.05f" % test_acc
+	print precision_recall_fscore_support(logistic.predict(X_test), y_test, average='binary')
+	accuracy.append(test_acc)
+print "-"
+print "Average accuracy %.05f" %(sum(accuracy)/len(accuracy))
